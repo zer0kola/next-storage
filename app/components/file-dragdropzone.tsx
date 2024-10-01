4c949b30d6f82296a36b5be5a958f9b863f46547
zer0kola <zer0kola321@gmail.com>
2024-10-02 03:13:03 +0900

    feat: 파일 드래그 앤 드롭 기능 추가 및 .idea 설정 파일 생성

--- app/components/file-dragdropzone.tsx
+++ app/components/file-dragdropzone.tsx
@@ -9,6 +9,8 @@ import { useDropzone } from "react-dropzone";
 
 export default function FileDragDropZone() {
   const fileRef = useRef(null);
+
+  // 파일 업로드 Mutation
   const uploadImageMutation = useMutation({
     mutationFn: uploadFile,
     onSuccess: () => {
@@ -18,6 +20,8 @@ export default function FileDragDropZone() {
     },
   });
 
+  // react-dropzone의 onDrop 콜백 함수
+  // dropzone에 파일을 놓았을 때 실행됨
   const onDrop = useCallback(async (acceptedFiles) => {
     if (acceptedFiles.length > 0) {
       const formData = new FormData();
@@ -30,6 +34,10 @@ export default function FileDragDropZone() {
     }
   }, []);
 
+  // react-dropzone의 hook
+  // getRootProps: dropzone의 root 엘리먼트에 필요한 props
+  // getInputProps: dropzone의 input 엘리먼트에 필요한 props
+  // isDragActive: dropzone에 파일을 놓았는지 여부
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     multiple: true,
